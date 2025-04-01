d9ac728b6c31c7f66505614c98041e7516cc37a2
Ardi <ardent628@gmail.com>
2025-03-31 19:19:50 -0500

    tabs -> spaces

--- docs/overrides/hooks/shortcodes.py
+++ docs/overrides/hooks/shortcodes.py
@@ -9,44 +9,44 @@ from mkdocs.structure.pages import Page
 from re import Match
 
 def on_page_markdown(
-	markdown: str, *, page: Page, config: MkDocsConfig, files: Files
+    markdown: str, *, page: Page, config: MkDocsConfig, files: Files
 ):
-	# Replace callback
-	def replace(match: Match):
-		type, args = match.groups()
-		args = args.strip()
+    # Replace callback
+    def replace(match: Match):
+        type, args = match.groups()
+        args = args.strip()
 
-		if type == "client": return f'<a href="/light/api/#tag-client" target="_blank"><span class="md-tag md-tag-icon md-tag--client">Client</span></a>'
-		elif type == "server": return f'<a href="/light/api/#tag-server" target="_blank"><span class="md-tag md-tag-icon md-tag--server">Server</span></a>'
-		elif type == "shared": return f'<a href="/light/api/#tag-shared" target="_blank"><span class="md-tag md-tag-icon md-tag--shared">Shared</span></a>'
-		elif type == "sync": return f'<a href="/light/api/#tag-sync" target="_blank"><span class="md-tag md-tag-icon md-tag--sync">Synchronous</span></a>'
-		elif type == "async": return f'<a href="/light/api/#tag-async" target="_blank"><span class="md-tag md-tag-icon md-tag--async">Yielding</span></a>'
-		elif type == "errors": return f'<a href="/light/api/#tag-errors" target="_blank"><span class="md-tag md-tag-icon md-tag--errors">Errors</span></a>'
-		elif type == "experimental": return f'<a href="/light/api/#tag-experimental" target="_blank"><span class="md-tag md-tag-icon md-tag--experimental">Experimental</span></a>'
-		elif type == "internal": return f'<a href="/light/api/#tag-internal" target="_blank"><span class="md-tag md-tag-icon md-tag--internal">Internal</span></a>'
+        if type == "client": return f'<a href="/light/api/#tag-client" target="_blank"><span class="md-tag md-tag-icon md-tag--client">Client</span></a>'
+        elif type == "server": return f'<a href="/light/api/#tag-server" target="_blank"><span class="md-tag md-tag-icon md-tag--server">Server</span></a>'
+        elif type == "shared": return f'<a href="/light/api/#tag-shared" target="_blank"><span class="md-tag md-tag-icon md-tag--shared">Shared</span></a>'
+        elif type == "sync": return f'<a href="/light/api/#tag-sync" target="_blank"><span class="md-tag md-tag-icon md-tag--sync">Synchronous</span></a>'
+        elif type == "async": return f'<a href="/light/api/#tag-async" target="_blank"><span class="md-tag md-tag-icon md-tag--async">Yielding</span></a>'
+        elif type == "errors": return f'<a href="/light/api/#tag-errors" target="_blank"><span class="md-tag md-tag-icon md-tag--errors">Errors</span></a>'
+        elif type == "experimental": return f'<a href="/light/api/#tag-experimental" target="_blank"><span class="md-tag md-tag-icon md-tag--experimental">Experimental</span></a>'
+        elif type == "internal": return f'<a href="/light/api/#tag-internal" target="_blank"><span class="md-tag md-tag-icon md-tag--internal">Internal</span></a>'
 
-		# Otherwise, raise an error
-		raise RuntimeError(f"Unknown shortcode: {type}")
+        # Otherwise, raise an error
+        raise RuntimeError(f"Unknown shortcode: {type}")
 
-	# Find and replace all external asset URLs in current page
-	return re.sub(
-		r"<!-- b:(\w+)(.*?) -->",
-		replace, markdown, flags = re.I | re.M
-	)
+    # Find and replace all external asset URLs in current page
+    return re.sub(
+        r"<!-- b:(\w+)(.*?) -->",
+        replace, markdown, flags = re.I | re.M
+    )
 
 # -----------------------------------------------------------------------------
 
 # Resolve path of file relative to given page - the posixpath always includes
 # one additional level of `..` which we need to remove
 def _resolve_path(path: str, page: Page, files: Files):
-	path, anchor, *_ = f"{path}#".split("#")
-	path = _resolve(files.get_file_from_path(path), page)
-	return "#".join([path, anchor]) if anchor else path
+    path, anchor, *_ = f"{path}#".split("#")
+    path = _resolve(files.get_file_from_path(path), page)
+    return "#".join([path, anchor]) if anchor else path
 
 # Resolve path of file relative to given page - the posixpath always includes
 # one additional level of `..` which we need to remove
 def _resolve(file: File, page: Page):
-	path = posixpath.relpath(file.src_uri, page.file.src_uri)
-	return posixpath.sep.join(path.split(posixpath.sep)[1:])
+    path = posixpath.relpath(file.src_uri, page.file.src_uri)
+    return posixpath.sep.join(path.split(posixpath.sep)[1:])
 
 # -----------------------------------------------------------------------------
